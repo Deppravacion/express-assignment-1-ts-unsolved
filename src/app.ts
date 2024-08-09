1de9d19845da2a57c8fa40e24f23006108083f4a
Tyson <deppravacion@gmail.com>
2024-08-09 07:22:14 -0500

    UPDATE: post method

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { prisma } from "../prisma/prisma-instance";
 import { errorHandleMiddleware } from "./error-handler";
 import "express-async-errors";
+import { stringify } from "querystring";
 
 const app = express();
 app.use(express.json());
@@ -51,6 +52,100 @@ app.delete("/dogs/:id", async (req, res) => {
     where: { id },
   });
   return res.json(dog).status(200);
+});
+
+//### Create Endpoint
+
+const errorAgeCheck = (
+  age: number | string | null | undefined
+) => {
+  if (typeof age !== "number") {
+    return "AGE should be a number";
+  }
+  return null;
+};
+
+const errorNameCheck = (
+  name: number | string | null | undefined
+) => {
+  if (typeof name !== "string") {
+    return " NAME should be a string";
+  }
+  return null;
+};
+
+const errorBreedCheck = (
+  breed: number | string | null | undefined
+) => {
+  if (typeof breed !== "string") {
+    return "BREED should be a string";
+  }
+  return null;
+};
+
+const errorDescriptionCheck = (
+  description: number | string | null | undefined
+) => {
+  if (typeof description !== "string") {
+    return "DESCRIPTION should be a string";
+  }
+};
+
+app.post("/dogs", async (req, res) => {
+  const { name, age, breed, description } = req.body;
+  const ageError = errorAgeCheck(age);
+  const nameError = errorNameCheck(name);
+  const breedError = errorBreedCheck(breed);
+  const descriptionError =
+    errorDescriptionCheck(description);
+
+  const properties = [
+    "name",
+    "age",
+    "breed",
+    "description",
+  ];
+  const invalidProperties = [];
+  for (const property in req.body) {
+    if (!properties.includes(property)) {
+      invalidProperties.push(property);
+    }
+  }
+
+  if (invalidProperties.length > 0) {
+    return res.status(400).send({
+      errors: invalidProperties.map(
+        (key) => `"${key}" is not a valid key.`
+      ),
+    });
+  }
+
+  const errors = [];
+
+  if (ageError !== null) {
+    errors.push(ageError);
+  }
+  if (breedError !== null) {
+    errors.push(breedError);
+  }
+  if (nameError !== null) {
+    errors.push(nameError);
+  }
+  if (descriptionError !== null) {
+    errors.push(descriptionError);
+  }
+
+  try {
+    const createDog = await prisma.dog.create({
+      data: {
+        name,
+        age,
+        breed,
+        description,
+      },
+    });
+    res.status(201).send(createDog);
+  } catch (error) {}
 });
 
 // all your code should go above this line
