2d4ab00c4ea5803d353543784a33d11dbcb8e616
Tyson <deppravacion@gmail.com>
2024-08-16 06:39:25 -0500

    Refactor: Zod lessons

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { prisma } from "../prisma/prisma-instance";
 import { errorHandleMiddleware } from "./error-handler";
 import "express-async-errors";
-// import { stringify } from "querystring";
+import { validateRequest } from "zod-express-middleware";
+import { z } from "zod";
 
 const app = express();
 app.use(express.json());
@@ -89,76 +90,87 @@ function errorCheckDescription(description: string) {
 }
 
 //## CREATE Endpoint
-app.post("/dogs", async (req, res) => {
-  console.log("Received request:", req.body);
+app.post(
+  "/dogs",
+  validateRequest({
+    body: z.object({
+      name: z.string(),
+      age: z.number(),
+      breed: z.string(),
+      description: z.string(),
+    }),
+  }),
+  async (req, res) => {
+    console.log("Received request:", req.body);
 
-  const { name, age, breed, description } = req.body;
-  const ageError = errorCheckAge(age);
-  const nameError = errorCheckName(name);
-  const breedError = errorCheckBreed(breed);
-  const descriptionError =
-    errorCheckDescription(description);
+    const { name, age, breed, description } = req.body;
+    const ageError = errorCheckAge(age);
+    const nameError = errorCheckName(name);
+    const breedError = errorCheckBreed(breed);
+    const descriptionError =
+      errorCheckDescription(description);
 
-  const validProperties = [
-    "name",
-    "age",
-    "breed",
-    "description",
-  ];
-  const invalidProperties = [];
+    const validProperties = [
+      "name",
+      "age",
+      "breed",
+      "description",
+    ];
+    const invalidProperties = [];
 
-  for (const key in req.body) {
-    if (!validProperties.includes(key)) {
-      invalidProperties.push(key);
+    for (const key in req.body) {
+      if (!validProperties.includes(key)) {
+        invalidProperties.push(key);
+      }
+    }
+
+    if (invalidProperties.length > 0) {
+      return res.status(400).send({
+        errors: invalidProperties.map(
+          (key) => `'${key}' is not a valid key`
+        ),
+      });
+    }
+
+    const errors = [];
+
+    if (ageError !== null) {
+      errors.push(ageError);
+    }
+    if (breedError !== null) {
+      errors.push(breedError);
+    }
+    if (nameError !== null) {
+      errors.push(nameError);
+    }
+    if (descriptionError !== null) {
+      errors.push(descriptionError);
+    }
+
+    if (errors.length > 0) {
+      return res.status(400).send({ errors });
+    }
+
+    try {
+      const dogData = {
+        name,
+        age,
+        breed,
+        description,
+      };
+
+      const createDog = await prisma.dog.create({
+        data: dogData,
+      });
+      res.status(201).send(createDog);
+    } catch (error) {
+      console.error("Database error:", error);
+      res
+        .status(500)
+        .send({ error: "Internal Server Error" });
     }
   }
-
-  if (invalidProperties.length > 0) {
-    return res.status(400).send({
-      errors: invalidProperties.map(
-        (key) => `'${key}' is not a valid key`
-      ),
-    });
-  }
-
-  const errors = [];
-
-  if (ageError !== null) {
-    errors.push(ageError);
-  }
-  if (breedError !== null) {
-    errors.push(breedError);
-  }
-  if (nameError !== null) {
-    errors.push(nameError);
-  }
-  if (descriptionError !== null) {
-    errors.push(descriptionError);
-  }
-
-  if (errors.length > 0) {
-    return res.status(400).send({ errors });
-  }
-
-  try {
-    const dogData = {
-      name,
-      age,
-      breed,
-      description,
-    };
-
-    const createDog = await prisma.dog.create({
-      data: dogData,
-    });
-    res.status(201).send(createDog);
-  } catch (error) {
-    console.error("Database error:", error);
-    res
-      .status(500)
-      .send({ error: "Internal Server Error" });
-  }
-});
+);
 
 //### Update Endpoint
 
